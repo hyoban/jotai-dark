90ea60bb4c5f661ecd50fcc308c0e4117867f40c
Stephen Zhou <hi@hyoban.cc>
2024-03-09 16:42:37 +0800

    chore: update

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,3 @@
 import hyoban from 'eslint-config-hyoban'
 
-export default hyoban(
-  {},
-  {
-    files: ['**/package.json'],
-    rules: {
-      'package-json/valid-package-def': 'off',
-    },
-  },
-)
+export default hyoban()
